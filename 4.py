2df50b8cf82660dfffb02321e7da773e0794fbdf
md56n <47092603+md56n@users.noreply.github.com>
2019-10-14 19:14:12 -0500

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://scikit-learn.org/stable/auto_examples/cluster/plot_mini_batch_kmeans.html#sphx-glr-auto-examples-cluster-plot-mini-batch-kmeans-py'
+res = requests.get(url)
+html_page = res.content
+soup = BeautifulSoup(html_page, 'html.parser')
+text = soup.find_all(text=True)
+
+output = ''
+blacklist = [
+	'[document]',
+	'noscript',
+	'header',
+	'html',
+	'meta',
+	'head',
+	'input',
+	'script',
+	# there may be more elements you don't want, such as "style", etc.
+]
+
+for t in text:
+    if t.parent.name not in blacklist:
+        output += '{} '.format(t)
+
+print(output)
