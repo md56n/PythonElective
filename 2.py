83dc78b8462a882d8b66fdac2758aa14bc70bbd0
md56n <47092603+md56n@users.noreply.github.com>
2019-10-14 20:47:18 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
+#Declare lists and dictionary
 names = []
 keys = []
 myDict = {}
@@ -5,10 +6,12 @@ myDict = {}
 list = [( 'John', ('Physics', 80)) , ( 'Daniel', ('Science', 90)), ('John', ('Science', 95)), ('Mark',('Maths', 100)),
         ('Daniel', ('History', 75)),('Mark', ('Social', 95))]
 
+#loop through the list and add the values to the dictionary
 for (key, value) in list:
     if key in myDict:
         myDict[key].append(value)
     else:
         myDict[key] = [value]
 
+#print the dictionary
 print(myDict)
