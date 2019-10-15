268aafa07c033af22b12a5294d443d35ab683180
md56n <47092603+md56n@users.noreply.github.com>
2019-10-14 20:46:08 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,17 +1,18 @@
-
+#import library
 import sys
 
+#get input from user
 sample = input("Strings: ")
 list= []
 
-
+#if the character in the string is already listed then skip, else add it to the list
 for x in sample:
     if x in list:
         continue
     else:
         list.append(x)
 
-
+#print the list
 for i in list:
     sys.stdout.write(i)
 
